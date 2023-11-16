da1ad21d3ea7e7ff74f9187cffaf6f7b91266adc
FongBangJun <144409023+FongBangJun@users.noreply.github.com>
2023-11-16 12:07:54 +0800

    Add files via upload

---
+++ lesson_activity/store.js
@@ -0,0 +1,13 @@
+function showmessage() {
+alert("~Item Added To Cart~");
+}
+
+function bigImg(x) {
+x.style.height = "450px";
+x.style.width = "350px";
+}
+
+function normalImg(x) {
+x.style.height = "400px";
+x.style.width = "300px";
+}
